bdfb00408381fc22f57a32dbea4d21c8cceee227
Done Ribeiro <www.done_@hotmail.com>
2020-01-16 15:14:18 -0300

    User entity and resource

---
+++ src/main/java/com/done/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.done.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.done.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "99887766", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
