f20b0dc7b82e044c7c1b3d53bb954bee850c79f3
Done Ribeiro <www.done_@hotmail.com>
2020-01-18 16:50:44 -0300

    Service layer, component registration

---
+++ src/main/java/com/done/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.done.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.done.course.entities.User;
+import com.done.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
